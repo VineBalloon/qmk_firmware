6f1557c2adb2275995c405ff38e91e48f6b1bed8
VineBalloon <vincentchen2.033@gmail.com>
2018-08-12 01:11:09 +1000

    Fixed wrong keymap name

--- keyboards/planck/keymaps/vdefault/keymap.c
+++ keyboards/planck/keymaps/vdefault/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_mit(
     KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,    KC_F8,      KC_F9,      KC_F10,  KC_F11,  KC_F12,
     KC_TILD,    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_PIPE,
-    S(KC_LBRC), KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MVOLD, KC_MVOLU, KC_HOME,    KC_END,     KC_UNDS, KC_PLUS, S(KC_RBRC),
+    S(KC_LBRC), KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD,  KC_VOLU,  KC_HOME,    KC_END,     KC_UNDS, KC_PLUS, S(KC_RBRC),
     _______,    KC_BSPC, KC_DEL,  KC_ENT,  _______,      _______,       _______,    _______,    _______, _______, _______
 ),
 
