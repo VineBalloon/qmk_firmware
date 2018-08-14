8f877a72e70d7d85d4411549fbb684ff51504c24
VineBalloon <vincentchen2.033@gmail.com>
2018-08-15 02:31:30 +1000

    Fixing more things

--- keyboards/planck/keymaps/vdefault/keymap.c
+++ keyboards/planck/keymaps/vdefault/keymap.c
@@ -100,8 +100,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_mit(
     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
-    KC_DEL,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       _______,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_MINS,    KC_BSLS,
-    KC_LBRC,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,    KC_PGDN,    KC_PGUP,    KC_MUTE,    KC_EQL,     KC_RBRC,
+    KC_DEL,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_VOLU,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_MINS,    KC_BSLS,
+    KC_LBRC,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_VOLD,    KC_PGDN,    KC_PGUP,    KC_MUTE,    KC_EQL,     KC_RBRC,
     _______,    _______,    _______,    _______,    _______,        TO(_MOUSE),         _______,    _______,    _______,    _______,    _______
 ),
 
@@ -117,10 +117,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_MOUSE] = LAYOUT_planck_mit(
-    _______,    _______,    _______,    KC_MS_U,    _______,    _______,    _______,    _______,    _______,    _______,   _______,     _______,
-    _______,    _______,    KC_MS_L,    KC_MS_D,    KC_MS_R,    _______,    _______,    KC_BTN1,    KC_BTN2,    KC_BTN3,   KC_BTN4,     KC_BTN5,
-    KC_ACL0,    KC_ACL1,    KC_ACL2,    _______,    _______,    _______,    _______,    _______,    _______,    _______,   _______,     _______,
-    _______,    _______,    _______,    _______,    _______,         TG(_MOUSE),        _______,    KC_WH_L,    KC_WH_D,   KC_WH_U,     KC_WH_R
+    _______,    _______,    _______,    KC_MS_U,    _______,    _______,    _______,   _______,     _______,    _______,   _______,     _______,
+    _______,    _______,    KC_MS_L,    KC_MS_D,    KC_MS_R,    _______,    _______,   KC_BTN1,     KC_BTN2,    KC_BTN3,   KC_BTN4,     KC_BTN5,
+    KC_ACL0,    KC_ACL1,    KC_ACL2,    _______,    _______,    _______,    _______,   _______,     _______,    _______,   _______,     _______,
+    _______,    _______,    _______,    _______,    TO(_BASE),         _______,        TO(_BASE),   KC_WH_L,    KC_WH_D,   KC_WH_U,     KC_WH_R
 ),
 
 /* Plover layer (http://opensteno.org)
