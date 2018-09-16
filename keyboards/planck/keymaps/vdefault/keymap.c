b51c6ade841367db850b149474af5a4352675c48
VineBalloon <vincentchen2.033@gmail.com>
2018-09-16 13:32:14 +1000

    Added back missing volume controls

--- keyboards/planck/keymaps/vdefault/keymap.c
+++ keyboards/planck/keymaps/vdefault/keymap.c
@@ -82,8 +82,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_planck_mit(
     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
-    KC_DEL,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       _______,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_MINS,    KC_EQL,
-    _______,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,    KC_VOLD,    KC_VOLU,    KC_MUTE,    KC_LBRC,    KC_RBRC,
+    KC_DEL,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_VOLU,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_MINS,    KC_EQL,
+    _______,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_VOLD,    KC_VOLD,    KC_VOLU,    KC_MUTE,    KC_LBRC,    KC_RBRC,
     _______,    _______,    _______,    _______,    _______,        TO(_MOUSE),         _______,    _______,    _______,    _______,    _______
 ),
 
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
     KC_INS,     KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   KC_UNDS,    KC_PLUS, 
     _______,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     KC_LCBR,    KC_RCBR,
-    _______,    _______,    _______,    _______,    _______,        TO(_MOUSE),         _______,    _______,    _______,    _______,    KC_PSCR,
+    _______,    _______,    _______,    _______,    _______,        TO(_MOUSE),         _______,    _______,    _______,    _______,    KC_PSCR
 ),
 
 /* Mouse
